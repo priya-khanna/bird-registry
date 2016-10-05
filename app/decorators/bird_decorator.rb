46c52294e69bc027166c8b08dcd17ce0669e2270
Priya Khanna <kpriya1912@gmail.com>
2016-10-05 15:45:49 +0530

    Adds object id in bird decorator

--- app/decorators/bird_decorator.rb
+++ app/decorators/bird_decorator.rb
@@ -1,9 +1,13 @@
 require_dependency 'application_decorator'
 class BirdDecorator < ApplicationDecorator
 
+  def id
+    model.id.to_s
+  end
+
   protected
   def attributes_for_output
-    %w(name family continents added visible)
+    %w(id name family continents added visible)
   end
 
 end
